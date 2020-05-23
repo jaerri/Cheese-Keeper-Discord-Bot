168176c113979a8fdd06ea6cccea7a59b088d2c0
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-05-23 17:19:57 +0700

    Update scrapermodel.js

--- commands/scrapermodel.js
+++ commands/scrapermodel.js
@@ -3,10 +3,10 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    const [el] = await page.$x('xpath');
+    const [el] = await page.$x('//xpath');
     const Txt = await el.getProperty('textContent');
     const numba = await Txt.jsonValue();
     console.log(numba);
     browser.close();
 }
-nameoffunction('website link');
+nameoffunction('//website link');
