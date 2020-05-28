33ab70845ff7d53f8d20f6a184999eb782caf950
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-05-28 21:54:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ bot.login(token);
 
 bot.on('ready', () => { 
     console.log("bot on use it rn @Tresh Jærry#8892 @Tresh Jærry#8892 @Tresh Jærry#8892");
+    message.channel.send("Bot is updated!")
 });
 
 
