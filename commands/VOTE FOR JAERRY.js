0d11b0b4cf0dba49c096cc3ae75011508fa8e64d
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-05 17:22:26 +0700

    Update VOTE FOR JAERRY.js

--- commands/VOTE FOR JAERRY.js
+++ commands/VOTE FOR JAERRY.js
@@ -2,6 +2,6 @@ module.exports = {
     name: "VOTE",
     description: "vote",
     execute(message, args){
-      message.channel.send('**VOTE FOR JAERRY! VOTE FOR JAERRY! VOTE FOR JAERRY!** \n                  **THE DEMOCART SOCIALIST REPUBLIC!** \n                  *100% DEMOCRATIC GUARANNTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:');
+      message.channel.send('**VOTE FOR JAERRY! VOTE FOR JAERRY! VOTE FOR JAERRY!** \n                  **THE DEMOCRAT SOCIALIST REPUBLIC!** \n                  *100% DEMOCRATIC GUARANNTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:');
   }
 }
