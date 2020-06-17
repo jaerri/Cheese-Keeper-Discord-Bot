bcd2e37634122a29a31b5d5454107a838813b21a
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-17 20:12:17 +0700

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -4,7 +4,7 @@ module.exports = {
     execute(message, args){
         //How to random : "(const) (randomElement) = (array)[Math.floor(Math.random() * (array).length)];""
         //example
-        let answers = ['Hi!', 'Hello dumbass', 'Hello!', 'Privet!', 'eating ur mom', 'gayyyyy', 'stop this cmd is gay', 'lô con c*c']
+        let answers = ['Hi!', 'Hello dumbass', 'Hello!', 'Privet!', 'eating ur mom', 'gaayyyyy', 'stop this cmd is gay', 'what', 'stoopid stop u gay', 'bruh what', ' don\'t aaa', 'u fucking gay']
         let randomanswers = answers[Math.floor(Math.random() * answers.length)];
         message.channel.send(randomanswers);
     }
