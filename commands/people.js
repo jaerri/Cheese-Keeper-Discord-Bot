92b3e9dd8838e2e842339d9af902a3d61867fabe
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-19 11:52:05 +0700

    Update people.js

---
+++ commands/people.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "people",
+    description: "people",
+    execute(message, args){
+        const puppeteer = require('puppeteer');
+        async function people(url){
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(url);
+            const [el] = await page.$x('//xpath');
+            const Txt = await el.getProperty('textContent');
+            const numba = await Txt.jsonValue();
+            console.log(numba);
+            browser.close();
+        }
+
+        people('//website link');
+    }
+}
