fc24123117c9c2171e4ef26ed6968f514ea424e6
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-20 08:57:01 +0700

    Update VOTE.js

--- commands/VOTE.js
+++ commands/VOTE.js
@@ -5,8 +5,7 @@ module.exports = {
       function message(on)
       {
         if (on = true) {
-          var me = message.guild.members.cache.get("679948431103492098").nickname.toUpperCase();
-          message.channel.send(`**VOTE FOR ${me}! VOTE FOR ${me}! VOTE FOR ${me}!** \n                             **THE ${message.guild.name.toUpperCase()}!** \n                 *100% **COLOPITALISM** GUARANTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:`);
+          message.channel.send(`**VOTE FOR JAERRY! VOTE FOR JAERRY! VOTE FOR JAERRY!** \n                             **THE ${message.guild.name.toUpperCase()}!** \n                 *100% **COLOPITALISM** GUARANTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:`);
         }
       }
       message(true)
