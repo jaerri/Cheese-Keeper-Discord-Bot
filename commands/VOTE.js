94407d792c3f442b0963403c02280ccc33899d58
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-20 08:59:12 +0700

    Update VOTE.js

--- commands/VOTE.js
+++ commands/VOTE.js
@@ -1,13 +1,7 @@
 module.exports = {
-    name: "VOTE",
-    description: "vote",
-    execute(message, args){
-      function message(on)
-      {
-        if (on = true) {
-          message.channel.send(`**VOTE FOR JAERRY! VOTE FOR JAERRY! VOTE FOR JAERRY!** \n                             **THE ${message.guild.name.toUpperCase()}!** \n                 *100% **COLOPITALISM** GUARANTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:`);
-        }
-      }
-      message(true)
-    }
+  name: "VOTE",
+  description: "vote",
+  execute(message, args){
+    message.channel.send(`**VOTE FOR JAERRY! VOTE FOR JAERRY! VOTE FOR JAERRY!** \n                             **THE ${message.guild.name.toUpperCase()}!** \n                 *100% **COLOPITALISM** GUARANTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:`);
+  }
 }
