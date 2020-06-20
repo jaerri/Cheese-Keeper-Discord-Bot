53d9d84de9434b8ec317a3cd2e400c2e5c51e9bc
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-20 08:34:50 +0700

    Update VOTE.js

--- commands/VOTE.js
+++ commands/VOTE.js
@@ -2,7 +2,10 @@ module.exports = {
     name: "VOTE",
     description: "vote",
     execute(message, args){
+      var on = false
+      if (on = true) {
       var me = message.guild.members.cache.get("679948431103492098").nickname.toUpperCase();
       message.channel.send(`**VOTE FOR ${me}! VOTE FOR ${me}! VOTE FOR ${me}!** \n                             **THE ${message.guild.name.toUpperCase()}!** \n                 *100% **COLOPITALISM** GUARANTEED* \n                                         VOTE NOW!\n                          :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup: :thumbsup:`);
+      }
     }
 }
