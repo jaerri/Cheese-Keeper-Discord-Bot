14631f5372c4edb8c94570bd4e17acbeaf64c17f
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-06-22 20:58:09 +0700

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -5,8 +5,7 @@ module.exports = {
         var i;
         if (!args[1]) return;
         else {
-            var answers = [args[1], args[2], args[3], args[4], args[5], args[6]]
-            var answer = answers.join();
+            var answer = message.slice(1);
             for (i = 0; i<=4; i++) {     
                 message.channel.send(answer);
             } 
