c146e11e2c19f26602ff246f7c3e2f95f910a128
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-02 13:33:50 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -9,6 +9,7 @@ module.exports = {
       'https://discord.com/api/oauth2/authorize?client_id=706095024869474354&permissions=8&scope=bot')
       .setThumbnail('https://media.discordapp.net/attachments/696673595505639474/728131476557922354/png-transparent-emoji-question-mark-exclamation-mark-android-text-messaging-question-mark-text-logo-.png?width=475&height=475')
       .setTitle('Bot\'s Commands :')
+      .setURL('https://discord.js.org/#/')
       .setDescription('Click bot\'s name to get invite link')
       .addFields(
         { name: '!help :', value: 'The command you are using, specific command help coming soon!'},
