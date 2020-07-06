974da1e1ca39e47001f30ff02eae13d0f229b222
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-06 18:58:39 +0700

    Update randominvite.js

--- commands/randominvite.js
+++ commands/randominvite.js
@@ -19,7 +19,8 @@ module.exports = {
             var chunck = string.match(/.{0,6}/g);
             var chuncks = chunck.join("discord.gg/");
             var answer = `discord.gg/${chuncks}`
-            const finalAnswer = [answer.slice(0, 17), "\n", answer.slice(17)].join('');
+            var answerChunck = answer.match(/.{0,6}/g);
+            var finalAnswer = answerChunck.join("discord.gg/");
             message.channel.send(finalAnswer);
             browser.close();
         }
