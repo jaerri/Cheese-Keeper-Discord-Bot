53c7b2c314faeeded9d739db7bd4016dbd21443f
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-06 15:42:30 +0700

    .

---
+++ commands/randominvite.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "randominvite",
+    description: "random invite generator",
+    execute(message, args){
+       //code here
+  }
+}
--- index.js
+++ index.js
@@ -30,60 +30,6 @@ bot.on('message', message=>{
                 bot.commands.get("hello").execute(message, args);
             break;
 
-            case '.':
-            case '·': 
-            case '•':
-                bot.commands.get("dot").execute(message, args);
-            break;
-
-            case ',':
-            case '‚':
-                bot.commands.get("comma").execute(message, args);
-            break;
-
-            case ';':
-                bot.commands.get("semicolon").execute(message, args);
-            break;
-
-            case ':':
-                bot.commands.get("colon").execute(message, args);
-            break;
-
-            case '/': 
-            case '⁄':
-            case '\/':
-                bot.commands.get("forwardslash").execute(message, args);
-            break;
-
-            case "\\":
-                bot.commands.get("backslash").execute(message, args);
-            break;
-
-            case "'": 
-            case '’':
-                bot.commands.get("apostrophe").execute(message, args);
-            break;
-
-            case '"':
-                bot.commands.get("quotationmark").execute(message, args);
-            break;
-
-            case '-':
-                bot.commands.get("hyphen").execute(message, args);
-            break;
-
-            case '|':
-                bot.commands.get("verticalbar").execute(message, args);
-            break;
-
-            case '~':
-                bot.commands.get("tilde").execute(message, args);
-            break;
-
-            case '`':
-                bot.commands.get("backtick").execute(message, args);
-            break;
-
             case `${prefix}vote`:
                 bot.commands.get("VOTE").execute(message, args);
             break;
@@ -104,13 +50,86 @@ bot.on('message', message=>{
                 bot.commands.get("mention").execute(message, args);
             break;
 
+            case `${prefix}randominvite`:
+                bot.commands.get("randominvite").execute(message, args);
+            break;
+        }
+    }
+);
+
+const fs = require('fs');
+bot.characters = new Collection();
+const charactersFiles = fs.readdirSync('./characters/').filter(file => file.endsWith('.js'));
+for(const file of charactersFiles){
+    const characters = require(`./characters/${file}`);
+    bot.characters.set(command.name, command);
+}
+
+bot.on('message', message=>{
+    if (message.author.bot || message.channel.type == "dm") return;
+    const args = message.content.split(' ');
+        switch(args[0].toLowerCase()){
             case "æ":            
-                bot.commands.get("lowercaseæ").execute(message, args);
+                bot.characters.get("lowercaseæ").execute(message, args);
             break;
 
             case "Æ":            
-                bot.commands.get("uppercaseÆ").execute(message, args);
+                bot.characters.get("uppercaseÆ").execute(message, args);
             break;
+
+            case '.':
+            case '·': 
+            case '•':
+                bot.characters.get("dot").execute(message, args);
+            break;
+    
+            case ',':
+            case '‚':
+                bot.characters.get("comma").execute(message, args);
+            break;
+    
+            case ';':
+                bot.characters.get("semicolon").execute(message, args);
+            break;
+    
+            case ':':
+                bot.characters.get("colon").execute(message, args);
+            break;
+    
+            case '/': 
+            case '⁄':
+            case '\/':
+                bot.characters.get("forwardslash").execute(message, args);
+            break;
+    
+            case "\\":
+                bot.characters.get("backslash").execute(message, args);
+            break;
+    
+            case "'": 
+            case '’':
+                bot.characters.get("apostrophe").execute(message, args);
+            break;
+    
+            case '"':
+                bot.characters.get("quotationmark").execute(message, args);
+            break;
+    
+            case '-':
+                bot.characters.get("hyphen").execute(message, args);
+            break;
+    
+            case '|':
+                bot.characters.get("verticalbar").execute(message, args);
+            break;
+    
+            case '~':
+                bot.characters.get("tilde").execute(message, args);
+            break;
+    
+            case '`':
+                bot.characters.get("backtick").execute(message, args);
+            break;    
         }
     }
 );
