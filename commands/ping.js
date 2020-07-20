0d9c0b519966994c5b22797d242e5ce14313d0b6
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-20 15:20:16 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,11 +4,10 @@ module.exports = {
     execute(message, args) {
         message.channel.send("Pinging ...")
 			.then((msg) => { 
-                let ping = Date.now() - msg.createdTimestamp
-                var otherMessage 
-                if (ping > 90) {otherMessage = "o no"}
-                msg.edit("Pong : " + ping + 'ms')
-                message.channel.send(otherMessage) 
+                let ping = Date.now() - msg.createdTimestamp;
+                var otherMessage;
+                if (ping > 90) {otherMessage = "o no"; message.channel.send(otherMessage);}
+                msg.edit("Pong : " + ping + 'ms');              
 		});
     }
 }
