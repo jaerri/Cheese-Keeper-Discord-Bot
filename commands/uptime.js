183d841480e542df7728a8153f0d65f0e8b36d2c
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-20 21:23:50 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "uptime",
     description: "Show bot's uptime.",
     execute(message, args) {
-        const duration = moment.duration(client.uptime).format('D [days], H [hours], m [minutes], and s [seconds]');
-        message.channel.send(`${bot.user.username} has been up for ${duration}.`);
+        const duration = moment.duration(bot.uptime).format('D [days], H [hours], m [minutes], and s [seconds]');
+        message.channel.send(`${message.guild.me.user.username} has been up for ${duration}.`);
     }
 }
