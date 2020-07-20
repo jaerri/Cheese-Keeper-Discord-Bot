f381e872f2698385a405ffdb374982a51ac6cd32
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-20 16:05:18 +0700

    Update settings.js

--- commands/settings.js
+++ commands/settings.js
@@ -3,16 +3,16 @@ module.exports = {
     description: "Settings Command",
     execute(message, args){
         function prefixS() {
-            prefixes.set(message.guild.id, arg[2]);
-            message.channel.send(`Success! Prefix is now ${arg2}`)
+            prefixes.set(message.guild.id, arg[3]);
+            message.channel.send(`Success! Prefix is now ${arg3}`)
         }
 
         if (!message.member.hasPermission('ADMINISTRATOR')) {
             return message.channel.send(`${message.author} you need **Administrator** permission to use this command!`);}
         else {
-            const arg2 = args.slice(2).join(' ').toLowerCase();               
+            const arg3 = args.slice(3).join(' ').toLowerCase();               
             if (args[1] == "prefix") {
-                if (!args[2]) {return message.channel.send("`settings prefix`\nSet new prefix for bot in this server.");}               
+                if (!args[3]) {return message.channel.send("`settings prefix`\nSet new prefix for bot in this server.");}               
                 else {                  
                     prefixS();
                 }            
