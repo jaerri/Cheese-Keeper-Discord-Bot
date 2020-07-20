6d024ee62ffe4f4a6421d0989659693ef90fffd3
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-20 20:01:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const {Client, Collection} = require("discord.js");
 const fs = require('fs');
 const Keyv = require('keyv');
-const myAdapter = require('./sqlite');
 const config = require("./config.json");
 const bot = new Client();
 
 var globalPrefix = "!";
-const prefixes = new Keyv({ store: myAdapter }, 'sqlite:/./sqlite/guildSettings.db');
+const prefixes = new Keyv('sqlite:/./sqlite/guildSettings.db');
 bot.login(config.token);
 
 bot.on('ready', () => { 
