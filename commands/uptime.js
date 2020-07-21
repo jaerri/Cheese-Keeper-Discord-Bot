35f5588b8372d5b32155a29403aa4b3667d36af0
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-21 13:13:22 +0700

    Update uptime.js

--- commands/uptime.js
+++ commands/uptime.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "uptime",
     description: "Show bot's uptime.",
     execute(message, args) {
-        let totalSeconds = (client.uptime / 1000);
+        let totalSeconds = (bot.uptime / 1000);
         let days = Math.floor(totalSeconds / 86400);
         let hours = Math.floor(totalSeconds / 3600);
         totalSeconds %= 3600;
