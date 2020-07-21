cb48df0b9efa115c67f73012768b7e9f4f305081
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-21 21:37:00 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,22 +1,22 @@
 module.exports = {
 	name: "help",
 	description: "Help cmd",
-	execute(message, args, commandFiles) {
+	execute(message, args, botCommands) {
+		const {MessageEmbed} = require('discord.js');
+
 		if (args[1]) {
-			let desc = commandFiles.get(args[1]).description;
-			if (!desc) return message.channel.send("con cac may hoi cai deo gi vay tao deo hieu");					
-			const {MessageEmbed} = require('discord.js');
+			let desc = botCommands.get(args[1]);
+			if (!desc) return message.channel.send("con cac may hoi cai gi vay tao deo hieu");					
 			const smallEmbeds = new MessageEmbed()
 			.setTitle(args[1])
 			.setThumbnail('https://media.discordapp.net/attachments/696673595505639474/728131476557922354/png-transparent-emoji-question-mark-exclamation-mark-android-text-messaging-question-mark-text-logo-.png?width=475&height=475')
 			.setDescription('Command Help')
-			.addFields({name: "Description :", value: desc})
+			.addFields({name: "Description :", value: desc.description})
 			.setColor(message.guild.me.displayColor)
 			message.channel.send(smallEmbeds);
 		}
 		
 		if (!args[1]) {
-			const {MessageEmbed} = require('discord.js');
 			const mainHelpEmbed = new MessageEmbed()
 			.setAuthor(message.guild.me.user.username, null, 'https://discord.com/oauth2/authorize?client_id=706095024869474354&permissions=8&scope=bot')
 			.setThumbnail('https://media.discordapp.net/attachments/696673595505639474/728131476557922354/png-transparent-emoji-question-mark-exclamation-mark-android-text-messaging-question-mark-text-logo-.png?width=475&height=475')
