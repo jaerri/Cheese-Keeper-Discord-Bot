4703d03b0d9e9cc0d7fd3cb2f8ceb8de7ce9d5e7
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-21 21:52:29 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ module.exports = {
 		if (!args[1]) {
 			const mainHelpEmbed = new MessageEmbed()
 			.setAuthor(message.guild.me.user.username, null, 'https://discord.com/oauth2/authorize?client_id=706095024869474354&permissions=8&scope=bot')
-			.setThumbnail('https://media.discordapp.net/attachments/696673595505639474/728131476557922354/png-transparent-emoji-question-mark-exclamation-mark-android-text-messaging-question-mark-text-logo-.png?width=475&height=475')
+			.setThumbnail('https://media.discordapp.net/attachments/696673595505639474/735146822863159336/question-mark.png')
 			.setTitle('Bot\'s Commands :')
 			.setURL('https://discord.js.org/#/')
 			.setDescription('Click bot\'s name to get invite link')
