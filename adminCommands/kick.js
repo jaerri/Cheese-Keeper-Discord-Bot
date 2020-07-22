92c9dc4d5d3c595b3261d2bb15628b01073e6169
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-22 15:23:22 +0700

    Update kick.js

--- adminCommands/kick.js
+++ adminCommands/kick.js
@@ -5,9 +5,14 @@ module.exports = {
         if (!message.mentions) return message.channel.send("may bi ngu a mention thang nao de tao kick coi");
         else {
             let member = message.mentions.members.first();
-            member.kick();
+            try {
+                member.kick();
             message.channel.send("no chet roi");
             setTimeout(message.channel.send(`thang nay ne ${member}`), 700)
+            }
+            catch(err){
+                message.channel.send("tao deo kick duoc thang nay");
+            }          
         }
     }
 }
