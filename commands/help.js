ab3b1a2b26d60944c9132cd400997549e34c72b4
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-22 12:19:12 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -26,7 +26,7 @@ module.exports = {
 				normalCommands += `\`${commandFiles[i].slice(0, -3)}\`` + ' ';
 			}
 			var adminCommands = ``
-			for (var i = 0; i < adminCommands.length; i++) 
+			for (var i = 0; i < adminFiles.length; i++) 
 			{
 				adminCommands += `\`${adminFiles[i].slice(0, -3)}\`` + ' ';
 			}
