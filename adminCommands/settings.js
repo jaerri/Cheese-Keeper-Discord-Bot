64d0767efd04c006d6f3e4c408f6656bb8690e7a
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-22 12:23:54 +0700

    Update settings.js

--- adminCommands/settings.js
+++ adminCommands/settings.js
@@ -3,7 +3,7 @@ module.exports = {
     description: "Settings command, use without argument 1 to show help",
     execute(message, args){
         if (require("../config.json").settingsEnabled = "true") {
-            let arg3 = arg[3].toLowerCase();    
+            let arg3 = args[3].toLowerCase();    
             message.channel.send("Available settings : \n` ` \nType with settings command.");         
         }   
         else return message.channel.send("Settings are disabled by the bot's owner.");
