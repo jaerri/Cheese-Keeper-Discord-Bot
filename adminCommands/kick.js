af878586a59b09f11b1f21266f4cf6c08dc9d2e3
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-07-22 16:29:32 +0700

    Update kick.js

--- adminCommands/kick.js
+++ adminCommands/kick.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "kick",
     description: "Use and mention a user to kick them.",
     execute(message, args){
-        let member = message.mentions.member.first();
+        let member = message.mentions.first.member;
         if (!member) return message.channel.send("may bi ngu a mention thang nao de tao kick coi");
         else {
             function kick() {
