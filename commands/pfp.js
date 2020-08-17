f0cfd8307d7149a16425bedb6a451479acb979c6
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-08-17 11:28:29 +0700

    Update pfp.js

--- commands/pfp.js
+++ commands/pfp.js
@@ -3,9 +3,9 @@ module.exports = {
     description: "Use with the id of the user you want to get their profile picture or mention them, use without arg 1 to get your own pfp.",   
     alias: null,
     type: "normal",
-    execute(message, args, bot) {
+    execute(message, args, prefix, bot) {
         const {MessageEmbed} = require('discord.js');
-        const user = message.mentions.users.first() || bot.users.find(user => user.id === args[1]) || message.author;
+        const user = message.mentions.users.first() || bot.users.cache.find(user => user.id === args[1]) || message.author;
 
         if (!user) return message.channel.send("Unknown user!");
         const embed = new MessageEmbed()
