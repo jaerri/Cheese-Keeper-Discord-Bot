67ded370d4a6d0e62cd2d9b82304f6cf33ce4fd1
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-08-24 09:20:41 +0700

    Update count.js

--- commands/count.js
+++ commands/count.js
@@ -1,13 +1,14 @@
 module.exports = {
-    name: "restart",
-    description: "Restart command file.",   
+    name: "count",
+    description: "Count from 1 to 10",   
     alias: null,
-    type: "reset",
+    type: null,
     execute(message, args, prefix, bot, commandFiles, botCommands) {
         //if (message.guild.id === "728598542784135249") {
             for (var i = 0; i <= 10; i++) {
                 message.channel.send(i);
             }
+            let myRole = message.guild.roles.cache.find(role => role.name === "@everyone");
         //}
         //else return;
     }  
