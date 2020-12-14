12a830f94833a7a255fd4e8499b068527f1b1984
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2020-12-14 18:56:20 +0700

    .

--- functions/string.js
+++ functions/string.js
@@ -3,7 +3,7 @@ module.exports = {
         let out = "";
         let arr = string.split(" ");
         for (let word of arr) {
-            out += word[0].toUpperCase() + word.slice(1).toLowerCase()  +" ";
+            out += word[0].toUpperCase() + word.slice(1).toLowerCase()  + " ";
         }
         return out.trim();
     }
