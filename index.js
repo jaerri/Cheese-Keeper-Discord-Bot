7131ef231f10ca4f1bcdf076332e2f221fc3c28a
Soupasoupy <62021987+Soupasoupy@users.noreply.github.com>
2021-05-24 08:58:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ bot.commands = new Collection();
 bot.callbacks = new Collection(); 
 bot.cooldowns = new Collection();
 
-const prefix = config.prefix;
+const prefix = bot.configs.prefix;
 //const mongodburl = config.mongodburl;
 
-bot.login(config.token);
+bot.login(bot.configs.token);
 bot.on('ready', async () => { 
     console.log("Bot online!");
     let jerri = await bot.users.fetch("679948431103492098", false);
@@ -57,7 +57,7 @@ for (let folder of folders) {
     let commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js') && !file.startsWith("!"));
     let folderFiles = [];
     for (let file of commandFiles) {
-        if (!config.settingsEnabled && file.startsWith("settings.js")) continue;
+        if (!bot.configs.settingsEnabled && file.startsWith("settings.js")) continue;
         let command = require(`./commands/${folder}/${file}`);
         folderFiles.push(command.name);
         bot.commands.set(command.name, command);
@@ -70,57 +70,6 @@ for (const callbackFile of callbacks) {
     let callback = require(`./callbacks/${callbackFile}`);
     bot.callbacks.set(callback.name, callback);
 }
-
-bot.on("messageDelete", (deletedMsg) => {
-    if (config.logEnabled == true && !deletedMsg.content.substring(prefix.length).startsWith("delete")) {  
-        if (deletedMsg.content.length > 100) deletedMsg.content = deletedMsg.content.slice(0, 100);     
-        if (deletedMsg.guild.channels.cache.find(channel => channel.name === 'logs')) {     
-            if (deletedMsg.author === bot.user && deletedMsg.channel.name === "logs" && deletedMsg.embeds[0]) {    
-                if (deletedMsg.embeds[0].title === "Message Deleted") {
-                    deletedMsg.channel.send("Do not delete log messages.", {embed: deletedMsg.embeds[0]}); 
-                }                 
-            }  
-            if (deletedMsg.author.bot) return;
-            var embed = new MessageEmbed()
-                .attachFiles(["./Files/trashcan-icon.png"])
-                .setAuthor(deletedMsg.author.username, deletedMsg.author.avatarURL())
-                .setTitle("Message Deleted")
-                .setThumbnail('attachment://trashcan-icon.png')
-                .setDescription(`A message by ${deletedMsg.author} was deleted in ${deletedMsg.channel} :`)
-                .addField("Deleted message content :", deletedMsg.content)
-                .setColor('#FF0000')
-                .setTimestamp(deletedMsg.createdTimestamp)
-                .setFooter("Deleted message sent at ");
-            deletedMsg.guild.channels.cache.find(channel => channel.name === 'logs').send(embed);
-        }   
-        if (deletedMsg.author.bot) return;     
-        deletedMsg.channel.send(`A message by ${deletedMsg.author} was deleted. The message's content is : ||${deletedMsg.content}||`);
-    }   
-}); 
-
-bot.on('messageUpdate', (oldMsg, newMsg) => {
-    if (!oldMsg.author.bot && config.logEnabled == true && oldMsg.content != newMsg.content) {
-        if (oldMsg.content.length > 100) oldMsg.content = oldMsg.content.slice(0, 100)
-        if (newMsg.content.length > 100) newMsg.content = newMsg.content.slice(0, 100)
-        oldMsg.channel.send(`A message was edited by ${oldMsg.author}. Old message : ||${oldMsg}|| turns into : ||${newMsg}||`);
-        if (oldMsg.guild.channels.cache.find(channel => channel.name === 'logs')) { 
-            var embed = new MessageEmbed()
-                .attachFiles(["./Files/pencil-icon.png"])
-                .setAuthor(oldMsg.author.username, oldMsg.author.avatarURL())
-                .setTitle("Message Edited")     
-				.setThumbnail('attachment://pencil-icon.png')
-                .setDescription(`A message was edited by ${oldMsg.author} in ${oldMsg.channel} :   [Jump!](${oldMsg.url} 'Click to jump to message')`)
-                .addFields(
-                    { name: `Old message content :`, value: oldMsg.content },
-                    { name: `turns into :`, value: newMsg.content}
-                )
-                .setColor('#FF4500')
-                .setTimestamp(oldMsg.createdTimestamp)
-                .setFooter("Old message sent at ");
-            oldMsg.guild.channels.cache.find(channel => channel.name === 'logs').send(embed);
-        }          
-    }
-});
 
 for (let callback of bot.callbacks.keys()) {
     bot.on(callback, async (param, param2) => {
